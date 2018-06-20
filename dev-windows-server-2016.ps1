6c62b2de5397f6c823476fd20cdae51bab6fc484
Garry <garry.pilkington.websites@gmail.com>
2018-06-20 10:03:59 +0100

    Added server file

---
+++ dev-windows-server-2016.ps1
@@ -0,0 +1,22 @@
+# Description: Boxstarter Script  
+# Author: Microsoft
+# Common dev settings for web dev
+
+Disable-UAC
+
+#--- Windows Features ---
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+
+#--- File Explorer Settings ---
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+
+#--- Browsers ---
+choco install -y googlechrome
+choco install -y firefox
+
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
